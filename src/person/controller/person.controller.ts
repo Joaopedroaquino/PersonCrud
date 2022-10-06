36c88beb217f6bbd086edbe062b7c882042389a7
joaopedroaquino <aqerhack@gmail.com>
2022-10-06 15:16:44 -0300

    retornando array para teste de rota

--- src/person/controller/person.controller.ts
+++ src/person/controller/person.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('person')
-export class PersonController {}
+export class PersonController {
+    @Get()
+    getAll(){
+        return [1,2,3];
+    }
+}
