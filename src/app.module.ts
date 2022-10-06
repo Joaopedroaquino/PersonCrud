1fc1fb29329d0e799191cf3d802367a07accc47d
joaopedroaquino <aqerhack@gmail.com>
2022-10-06 15:05:30 -0300

    criando o modulo person

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PersonModule } from './person/person.module';
 
 @Module({
   imports: [
     // TypeOrmModule.forRoot({ // })
-  ],
+  PersonModule],
   controllers: [],
   providers: [],
 })
