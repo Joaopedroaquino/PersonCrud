a53cf8d32567f3cbeb665a5fb2e0535c88be1627
joaopedroaquino <aqerhack@gmail.com>
2022-10-06 16:33:46 -0300

    criacao da entidade pessoa

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonModule } from './person/person.module';
 
 @Module({
-  imports: [PersonModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      retryDelay: 3000,
+      retryAttempts: 10
+    }),  
+    
+    PersonModule],
   controllers: [],
   providers: [],
 })
