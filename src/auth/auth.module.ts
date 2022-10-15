519d96d4c3bb3e747321fb2310be8aedcf5c5ffa
joaopedroaquino <aqerhack@gmail.com>
2022-10-14 22:00:28 -0300

    ref 2.3

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,12 +8,12 @@ import { JwtStrategyService } from './jwt-strategy/jwt-strategy.service';
 
   imports: [
     JwtModule.register({
-      secret:'abcd123456',
-      signOptions:{
-        expiresIn: '60s' 
-    }
-  })],
+      secret: 'abcd123456',
+      signOptions: {
+        expiresIn: '60s'
+      }
+    })],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategyService]
 })
-export class AuthModule {}
+export class AuthModule { }
