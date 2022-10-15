cb01ce938ddc539ae6a2b650112a549d18c09a0f
joaopedroaquino <aqerhack@gmail.com>
2022-10-14 21:03:24 -0300

    adicionando o modul jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth/auth.controller';
 
 @Module({
+
+  imports: [
+    JwtModule.register({
+      secret:'abcd123456',
+      signOptions:{
+        expiresIn: '60s' 
+    }
+  })],
   controllers: [AuthController]
 })
 export class AuthModule {}
