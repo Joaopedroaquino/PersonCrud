0991d5a1b7fcf29939845c96966e875a0dbaa69a
joaopedroaquino <aqerhack@gmail.com>
2022-10-14 21:04:26 -0300

    iniciando o auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
 
@@ -11,6 +12,7 @@ import { AuthController } from './auth/auth.controller';
         expiresIn: '60s' 
     }
   })],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  providers: [AuthService]
 })
 export class AuthModule {}
