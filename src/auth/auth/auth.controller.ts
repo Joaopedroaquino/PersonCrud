be74548fa2087de48eb2dccdb81da6e83ccf53c3
joaopedroaquino <aqerhack@gmail.com>
2022-10-14 21:07:49 -0300

    criada a rota post

--- src/auth/auth/auth.controller.ts
+++ src/auth/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('login')
+    login(@Body() body){
+        return {token: this.authService.login( body.username, body.password)};
+    }
+    
+}
