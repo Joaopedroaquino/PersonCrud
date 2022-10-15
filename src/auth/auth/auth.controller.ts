8be5e448ef1755eaf488a0401e9c18bf9e53518e
joaopedroaquino <aqerhack@gmail.com>
2022-10-14 21:46:18 -0300

    iniciando a autorizacao

--- src/auth/auth/auth.controller.ts
+++ src/auth/auth/auth.controller.ts
@@ -1,5 +1,8 @@
 import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Role } from 'role.decorator';
+import { RoleGuard } from '../role.guard';
 import { AuthService } from './auth.service';
+import { JwtGuard } from './jwt.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -10,6 +13,8 @@ export class AuthController {
         return {token: this.authService.login( body.username, body.password)};
     }
     
+    @Role('admin')
+    @UseGuards(JwtGuard, RoleGuard)
     @Get('enter')
     test(@Req() req){ 
         console.log(req.user)
