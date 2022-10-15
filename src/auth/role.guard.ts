273a545adbcc7da372a951b3ecc40f57610d5a88
joaopedroaquino <aqerhack@gmail.com>
2022-10-14 21:38:59 -0300

    criado o role guard

--- src/auth/role.guard.ts
+++ src/auth/role.guard.ts
@@ -1,11 +1,21 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector){}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const role = this.reflector.get('role', context.getHandler());
+    if(!role){
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    return user.role == role;
   }
 }
