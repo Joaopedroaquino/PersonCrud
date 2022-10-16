c41f04fc53b154383f621aa8bf32a1f8d0a903d4
Joao Pedro Aquino <aqerhack@gmail.com>
2022-10-16 16:53:34 -0300

    add secret key/ auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,14 @@ import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { jwtConstants } from './auth/constants';
 import { LocalStrategy } from './auth/local-strategy/local-strategy.service';
 import { JwtStrategyService } from './jwt-strategy/jwt-strategy.service';
 
 @Module({
   imports: [
     JwtModule.register({
-      secret: 'abcd123456',
+      secret: jwtConstants.secret,
       signOptions: {
         expiresIn: '60s'
       }
