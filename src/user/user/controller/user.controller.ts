dc6a1fb1e887a5dcea4cb36573aae4a1fa19aa51
Joao Pedro Aquino <aqerhack@gmail.com>
2022-10-16 12:04:48 -0300

    init rota get no controller

--- src/user/user/controller/user.controller.ts
+++ src/user/user/controller/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService){}
+
+
+    @Get()
+    getAll(){
+        return this.userService.findAll()
+    }
 }
