f6790bfafb52ec02f6d93094ce2f8aa582e48345
Joao Pedro Aquino <aqerhack@gmail.com>
2022-10-16 12:02:50 -0300

    init inj service no controller

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user/controller/user.controller';
+import { User } from './user/entities/user.entity';
 import { UserService } from './user/services/user.service';
 
 @Module({
+  imports:[ TypeOrmModule.forFeature([User, ])],
+
   controllers: [UserController],
   providers: [UserService]
 })
